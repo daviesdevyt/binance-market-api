ee57f637e69e898d15dd2d024d5defc56c445c60
unknown <davidonzan@gmail.com>
2022-09-01 14:46:49 +0100

    Refactor

--- app.py
+++ app.py
@@ -63,40 +63,35 @@ def exchange_info():
     return handle_request("GET", "/fapi/v1/exchangeInfo")
 
 
-@app.get("/market-order-book/{symbol}")
-def market_order_book(symbol: str, limit: Optional[int] = 500):
+@app.get("/market-order-book/{pair}")
+def market_order_book(pair: str, limit: Optional[int] = 500):
     if limit not in [5, 10, 20, 50, 100, 500, 1000]:
         return {"Error": "Valid limits are [5, 10, 20, 50, 100, 500, 1000]"}
-    data = handle_request("GET", f"/fapi/v1/depth", get_params(symbol=symbol, limit=limit))
-    if not data.get("Error"):
-        data["tx-time"] = data["T"]
-        data.pop("T")
-        data.pop("E")
-
-    return 
+    data = handle_request("GET", f"/fapi/v1/depth", get_params(symbol=pair, limit=limit))
+    return data
 
 
-@app.get("/trades/{symbol}")
-def trades(symbol: str, limit: Optional[int] = 500):
+@app.get("/trades/{pair}")
+def trades(pair: str, limit: Optional[int] = 500):
     if limit > 1000:
         return {"Error": "Maximum limit is 1000"}
-    return handle_request("GET", "/fapi/v1/trades", get_params(symbol=symbol, limit=limit))
+    return handle_request("GET", "/fapi/v1/trades", get_params(symbol=pair, limit=limit))
 
 
-# @app.get("/historical-trades/{symbol}")
-# def historical_trades(symbol: str, fromid: Optional[int] = None, limit: Optional[int] = 500):
+# @app.get("/historical-trades/{pair}")
+# def historical_trades(pair: str, fromid: Optional[int] = None, limit: Optional[int] = 500):
 #     if limit > 1000:
 #         return {'Error': "Maximum limit is 1000"}
-#     return handle_request("GET", "/fapi/v1/historicalTrades", get_params(symbol=symbol, limit=limit, fromId=fromid))
+#     return handle_request("GET", "/fapi/v1/historicalTrades", get_params(symbol=pair, limit=limit, fromId=fromid))
 
 
-@app.get("/candlestick/{symbol}/{interval}")
-def candlestick(symbol: str, interval: str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 500):
+@app.get("/candlestick/{pair}/{interval}")
+def candlestick(pair: str, interval: str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 500):
     if interval not in PERIODS:
         return {'Error': "Interval is invalid. Allowed intervals are "+str(PERIODS)}
     if limit > 1500:
         return {'Error': "Maximum limit is 1500"}
-    data = handle_request("GET", "/fapi/v1/klines", get_params(symbol=symbol,
+    data = handle_request("GET", "/fapi/v1/klines", get_params(symbol=pair,
                           interval=interval, startTime=startTime, endTime=endTime, limit=limit))
     keys = ["open-time", "open", "high", "low", "close", "volume", "close-time", "quote-asset-volume",
                 "number-of-trades", "taker-buy-base-asset-volume", "taker-buy-quote-asset-volume"]
@@ -134,85 +129,85 @@ def mark_price_candlestick(pair: str, interval: str, startTime: Optional[int]=No
     if limit > 1500:
         return {'Error': "Maximum limit is 1500"}
     keys = ["open-time", "open", "high", "low", "close", "ignore", "close-time"]
-    data = handle_request("GET", "/fapi/v1/markPriceKlines", get_params(pair=pair, interval=interval, startTime=startTime, endTime=endTime, limit=limit))
+    data = handle_request("GET", "/fapi/v1/markPriceKlines", get_params(symbol=pair, interval=interval, startTime=startTime, endTime=endTime, limit=limit))
     return arr2dict(data, keys)
 
 @app.get("/funding-rate")
-def get_funding_rate(symbol:Optional[str]=None,  startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 100):
+def get_funding_rate(pair:Optional[str]=None,  startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 100):
     if limit > 1000:
         return {'Error': "Maximum limit is 1000"}
-    return handle_request("GET", '/fapi/v1/fundingRate', get_params(symbol=symbol, startTime=startTime, endTime=endTime, limit=limit))
+    return handle_request("GET", '/fapi/v1/fundingRate', get_params(symbol=pair, startTime=startTime, endTime=endTime, limit=limit))
 
 @app.get("/24hr-stats")
-def daily_stats(symbol:Optional[str]=None ):
-    return handle_request("GET", '/fapi/v1/ticker/24hr', get_params(symbol=symbol))
+def daily_stats(pair:Optional[str]=None ):
+    return handle_request("GET", '/fapi/v1/ticker/24hr', get_params(symbol=pair))
 
 @app.get("/price")
-def get_price(symbol:Optional[str]=None):
-    return handle_request("GET", '/fapi/v1/ticker/price', get_params(symbol=symbol))
+def get_price(pair:Optional[str]=None):
+    return handle_request("GET", '/fapi/v1/ticker/price', get_params(symbol=pair))
 
 @app.get("/order-book")
-def order_book(symbol:Optional[str]=None):
-    return handle_request("GET", '/fapi/v1/ticker/bookTicker', get_params(symbol=symbol))
+def order_book(pair:Optional[str]=None):
+    return handle_request("GET", '/fapi/v1/ticker/bookTicker', get_params(symbol=pair))
 
-@app.get("/open-interest/{symbol}")
-def open_interest(symbol:str):
-    return handle_request("GET", '/fapi/v1/openInterest', get_params(symbol=symbol))
+@app.get("/open-interest/{pair}")
+def open_interest(pair:str):
+    return handle_request("GET", '/fapi/v1/openInterest', get_params(symbol=pair))
 
-@app.get("/open-interest-hist/{symbol}/{period}")
-def open_inyterest_hist(symbol:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
+@app.get("/open-interest-hist/{pair}/{period}")
+def open_inyterest_hist(pair:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
     if period not in PERIODS:
         return {'Error': "Period is invalid. Allowed periods are "+str(PERIODS)}
     if limit > 500:
         return {'Error': "Maximum limit is 500"}
-    return handle_request("GET", "/futures/data/openInterestHist", get_params(symbol=symbol, period=period, startTime=startTime, endTime=endTime, limit=limit))
+    return handle_request("GET", "/futures/data/openInterestHist", get_params(symbol=pair, period=period, startTime=startTime, endTime=endTime, limit=limit))
 
-@app.get("/top-long-short-account-ratio/{symbol}/{period}")
-def top_long_short_account_ratio(symbol:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
+@app.get("/top-long-short-account-ratio/{pair}/{period}")
+def top_long_short_account_ratio(pair:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
     if period not in PERIODS:
         return {'Error': "Period is invalid. Allowed periods are "+str(PERIODS)}
     if limit > 500:
         return {'Error': "Maximum limit is 500"}
-    return handle_request("GET", "/futures/data/topLongShortAccountRatio", get_params(symbol=symbol, period=period, startTime=startTime, endTime=endTime, limit=limit))
+    return handle_request("GET", "/futures/data/topLongShortAccountRatio", get_params(symbol=pair, period=period, startTime=startTime, endTime=endTime, limit=limit))
 
-@app.get("/top-long-short-position-ratio/{symbol}/{period}")
-def top_long_short_position_ratio(symbol:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
+@app.get("/top-long-short-position-ratio/{pair}/{period}")
+def top_long_short_position_ratio(pair:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
     if period not in PERIODS:
         return {'Error': "Period is invalid. Allowed periods are "+str(PERIODS)}
     if limit > 500:
         return {'Error': "Maximum limit is 500"}
-    return handle_request("GET", "/futures/data/topLongShortPositionRatio", get_params(symbol=symbol, period=period, startTime=startTime, endTime=endTime, limit=limit))
+    return handle_request("GET", "/futures/data/topLongShortPositionRatio", get_params(symbol=pair, period=period, startTime=startTime, endTime=endTime, limit=limit))
 
-@app.get("/global-long-short-account-ratio/{symbol}/{period}")
-def global_long_short_account_ratio(symbol:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
+@app.get("/global-long-short-account-ratio/{pair}/{period}")
+def global_long_short_account_ratio(pair:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
     if period not in PERIODS:
         return {'Error': "Period is invalid. Allowed periods are "+str(PERIODS)}
     if limit > 500:
         return {'Error': "Maximum limit is 500"}
-    return handle_request("GET", "/futures/data/globalLongShortAccountRatio", get_params(symbol=symbol, period=period, startTime=startTime, endTime=endTime, limit=limit))
+    return handle_request("GET", "/futures/data/globalLongShortAccountRatio", get_params(symbol=pair, period=period, startTime=startTime, endTime=endTime, limit=limit))
 
-@app.get("/taker-long-short-ratio/{symbol}/{period}")
-def taker_long_short_ratio(symbol:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
+@app.get("/taker-long-short-ratio/{pair}/{period}")
+def taker_long_short_ratio(pair:str, period:str, startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 30):
     if period not in PERIODS:
         return {'Error': "Period is invalid. Allowed periods are "+str(PERIODS)}
     if limit > 500:
         return {'Error': "Maximum limit is 500"}
-    return handle_request("GET", "/futures/data/takerlongshortRatio", get_params(symbol=symbol, period=period, startTime=startTime, endTime=endTime, limit=limit))
+    return handle_request("GET", "/futures/data/takerlongshortRatio", get_params(symbol=pair, period=period, startTime=startTime, endTime=endTime, limit=limit))
 
 @app.get("/blvt-nav-candlestick")
-def BLVT_NAV_candlestick(symbol:str, interval:str,  startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 500):
+def BLVT_NAV_candlestick(pair:str, interval:str,  startTime: Optional[int]=None, endTime: Optional[int]=None, limit: Optional[int] = 500):
     if limit > 1000:
         return {'Error': "Maximum limit is 500"}
     keys = ["open-time", "open", "high", "low", "close", "real-leverage", "close-time", "ignore", "number-of-nav-update"]
-    data = handle_request("GET", "/fapi/v1/lvtKlines", get_params(symbol=symbol, interval=interval, startTime=startTime, endTime=endTime, limit=limit))
+    data = handle_request("GET", "/fapi/v1/lvtKlines", get_params(symbol=pair, interval=interval, startTime=startTime, endTime=endTime, limit=limit))
     return arr2dict(data, keys)
 
 @app.get("/index-info")
-def index_info(symbol:Optional[str]=None):
-    return handle_request("GET", "/fapi/v1/indexInfo", get_params(symbol=symbol))
+def index_info(pair:Optional[str]=None):
+    return handle_request("GET", "/fapi/v1/indexInfo", get_params(symbol=pair))
 
 @app.get("/index-info")
-def index_info(symbol:Optional[str]=None):
-    return handle_request("GET", "/fapi/v1/assetIndex", get_params(symbol=symbol))
+def index_info(pair:Optional[str]=None):
+    return handle_request("GET", "/fapi/v1/assetIndex", get_params(symbol=pair))
 
 
